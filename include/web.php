99da7333a18e72fbeb733c502195b2b418d1198a
Yousaf-Sadiq <yousafsadiq445@gmail.com>
2024-04-02 00:32:53 +0500

    2-4-24 eshop work layout setting

---
+++ include/web.php
@@ -0,0 +1,7 @@
+<?php 
+
+// main domain route 
+define("ROOTPATH","http://localhost/");
+
+define("folder","PHP_WORK_10_30/ecom2/");
+?>
---
+++ layout/user/header.php
@@ -0,0 +1,102 @@
+<?php 
+
+
+        //                  layout/../
+require_once dirname(__DIR__)."/../include/web.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<!-- Mirrored from wpthemesgrid.com/themes/eshop/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 28 Mar 2024 20:09:37 GMT -->
+<head>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="copyright" content>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<title>Eshop - eCommerce HTML5 Template.</title>
+
+<link rel="icon" type="image/png" href="images/favicon.png">
+
+<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;display=swap" rel="stylesheet">
+
+
+<link rel="stylesheet" href="<?php echo ROOTPATH."".folder  ?>asset/css/bootstrap.css">
+
+<link rel="stylesheet" href="<?php echo ROOTPATH."".folder  ?>asset/css/magnific-popup.min.css">
+
+<link rel="stylesheet" href="<?php echo ROOTPATH."".folder  ?>asset/css/font-awesome.css">
+
+<link rel="stylesheet" href="<?php echo ROOTPATH."".folder  ?>asset/css/jquery.fancybox.min.css">
+
+<link rel="stylesheet" href="<?php echo ROOTPATH."".folder  ?>asset/css/themify-icons.css">
+
+<link rel="stylesheet" href="<?php echo ROOTPATH."".folder  ?>asset/css/niceselect.css">
+
+<link rel="stylesheet" href="<?php echo ROOTPATH."".folder  ?>asset/css/animate.css">
+
+<link rel="stylesheet" href="<?php echo ROOTPATH."".folder  ?>asset/css/flex-slider.min.css">
+
+<link rel="stylesheet" href="<?php echo ROOTPATH."".folder  ?>asset/css/owl-carousel.css">
+
+<link rel="stylesheet" href="<?php echo ROOTPATH."".folder  ?>asset/css/slicknav.min.css">
+
+<link rel="stylesheet" href="<?php echo ROOTPATH."".folder  ?>asset/css/reset.css">
+<link rel="stylesheet" href="<?php echo ROOTPATH."".folder  ?>asset/style.css">
+<link rel="stylesheet" href="<?php echo ROOTPATH."".folder  ?>asset/css/responsive.css">
+
+<link rel="stylesheet" href="<?php echo ROOTPATH."".folder  ?>asset/css/color/color1.css">
+
+
+
+
+
+
+
+
+
+
+
+<link rel="stylesheet" href="#" id="colors">
+</head>
+<body class="js">
+
+<!-- <div class="preloader">
+<div class="preloader-inner">
+<div class="preloader-icon">
+<span></span>
+<span></span>
+</div>
+</div>
+</div> -->
+
+
+<div class="color-plate ">
+<a class="color-plate-icon"><i class="ti-paint-bucket"></i></a>
+<h4>Eshop Colors</h4>
+<p>Here is some awesome color's available on Eshop Template.</p>
+<span class="color1"></span>
+<span class="color2"></span>
+<span class="color3"></span>
+<span class="color4"></span>
+<span class="color5"></span>
+<span class="color6"></span>
+<span class="color7"></span>
+<span class="color8"></span>
+<span class="color9"></span>
+<span class="color10"></span>
+<span class="color11"></span>
+<span class="color12"></span>
+</div>
+
+
+
+<?php 
+
+require_once  dirname(__FILE__)."/nav.php";
+
+?>
